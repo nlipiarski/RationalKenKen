c3fe582bcb4545fd057bd32d7b11095b61487306
Nathan Lipiarski <42iscool42@gmail.com>
2020-04-04 02:03:18 -0400

    Making fix that reduces number of possible solutions to puzzles

--- Grid.js
+++ Grid.js
@@ -22,8 +22,13 @@ function Grid(size) {
 		this.table.appendChild(row);
 	}
 
+	let groupsGood = false;
 	let groupPoints = this.makeGroups();
-	this.groups = new Array(groupPoints.length);
+	while (!areGroupsGood(groupPoints, this.fractions)) {
+		groupPoints = this.makeGroups();
+	}
+
+	this.groups = [];
 
 	for (let i = 0; i < groupPoints.length; i++) {
 		this.groups[i] = new Group(groupPoints[i], this.fractions, this.cells);
@@ -292,7 +297,7 @@ Grid.prototype.createSquareArray = function(size) {
 	return array;
 }
 
-/** Removes any entries that have been but in the cells in the html
+/** Removes any entries that have been put in the cells in the html
  * representation of this board.
  */
 Grid.prototype.clear = function() {
@@ -300,5 +305,165 @@ Grid.prototype.clear = function() {
 		for (let j = 0; j < this.size; j++) {
 			this.cells[i][j].clear();
 		}
+	}
+}
+
+/**
+ * Checks that there there is only one solution for the provided groups
+ * @param groups Array of groups of points
+ * @param fractions Assignment of fractions to the grid
+ * @return True if these groups have a unique assignment and false otherwise
+ */
+function areGroupsGood(groups, fractions) {
+	for (let i = 0; i < groups.length - 1; i++) {
+		for (let j = i + 1; j < groups.length; j++) {
+			if (canCommute(groups[i], groups[j], fractions)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Checks to see if between two groups there is the possibility to rearrange the
+ * contents and still have a valid solution
+ * @param group1 The first group of points
+ * @param group2 The second group of points
+ * @param fractions The assignment of fractions to the grid
+ */
+function canCommute(group1, group2, fractions) {
+	let group1XOverlap = [];
+	let group2XOverlap = [];
+	let group1YOverlap = [];
+	let group2YOverlap = [];
+
+	for (let i = 0; i < group1.length; i++) {
+		for (let j = 0; j < group2.length; j++) {
+			if (group1[i][0] === group2[j][0]) {
+				if (group1XOverlap.findIndex(createPointFinder(group1[i])) === -1) {
+					group1XOverlap.push(group1[i]);
+				}
+
+				if (group2XOverlap.findIndex(createPointFinder(group2[j])) === -1) {
+					group2XOverlap.push(group2[j]);	
+				}
+			}
+
+			if (group1[i][1] === group2[j][1]) {
+				if (group1YOverlap.findIndex(createPointFinder(group1[i])) === -1) {
+					group1YOverlap.push(group1[i]);
+				}
+
+				if (group2YOverlap.findIndex(createPointFinder(group2[j])) === -1) {
+					group2YOverlap.push(group2[j]);
+				}
+			}
+		}
+	}
+
+	return checkSubsetsOverlap(group1XOverlap, group2XOverlap, fractions) ||
+			checkSubsetsOverlap(group1YOverlap, group2YOverlap, fractions);
+}
+
+/**
+ * Checks whether all subsets of group1 when paired with any subset of group2 are
+ * able to commute within the group. Technically this check is stronger than needed
+ * but it seems to work I guess.
+ * @param group1 The first group of points
+ * @param group2 The second group of points
+ * @param fractions The assignment of fractions to the grid
+ * @return true if some subsets overlap and false otherwise
+ */
+function checkSubsetsOverlap(group1, group2, fractions) {
+	let minSize = Math.min(group1.length, group2.length);
+	for (let size = 2; size <= minSize; size++) {
+		let group1Sets = createSubsets(group1, size);
+		let group2Sets = createSubsets(group2, size);
+
+		for (let i = 0; i < group1Sets.length; i++) {
+			for (let j = 0; j < group2Sets.length; j++) {
+				if (setsEqual(group1Sets[i], group2Sets[j], fractions)) {
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Creates all subsets of size size of the array of points
+ * @param points The input points
+ * @param size Size of the subsets to create
+ * @return An array containing all subsets of size size.
+ */
+function createSubsets(points, size) {
+	let sets = [];
+	let currentContents = [0];
+
+	while (currentContents.length > 0) {
+		if (currentContents.length == size) {
+			let newSet = [];
+			for (let i = 0; i < size; i++) {
+				newSet.push(points[currentContents[i]]);
+			}
+			sets.push(newSet);
+
+			let j = size - 1;
+			for (; j >= 0 && currentContents[j] == points.length - size + j; j--) {
+				currentContents.pop();
+			}
+
+			if (j >= 0) {
+				currentContents[j] += 1;
+			}
+		} else {
+			const lastPoint = currentContents[currentContents.length - 1];
+			currentContents.push(lastPoint + 1);
+		}
+	}
+
+	return sets;
+}
+
+/**
+ * Brute force checks that the two sets of points contain the same fractions. The
+ * brute force here is ok because the sets are pretty much always going to be 
+ * smaller than 4.
+ * @param set1 The first set
+ * @param set2 The second set
+ * @param fractions The assignment of fractions to the grid
+ * @return true if the sets contain the same fractions and false otherwise
+ */
+function setsEqual(set1, set2, fractions) {
+	for (let i = 0; i < set1.length; i++) {
+		let curr1Frac = fractions[set1[i][0]][set1[i][1]];
+		let found = false;
+		for (let j = 0; j < set2.length; j++) {
+			let curr2Frac = fractions[set2[j][0]][set2[j][1]];
+			if (curr1Frac.equals(curr2Frac)) {
+				found = true;
+			}
+		}
+
+		if (!found) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Creates a function that returns true if the parameter equals the passed in
+ * point.  Used for array containment since comparison doesn't seem to work for
+ * arrays
+ */
+function createPointFinder(point) {
+	return function(element) {
+		return element[0] === point[0] && element[1] === point[1];
 	}
 }
